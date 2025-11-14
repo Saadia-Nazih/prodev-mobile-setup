5d3ee8f9e8e6a6f7346d816334b2c08e61130e82
Saadia Nazih <saadia.nazih92@gmail.com>
2025-11-14 10:56:45 +0100

    Aggiungo finalmente app-example per il checker

---
+++ prodev-mobile-app-0x00/app-example/constants/Colors.tsx
@@ -0,0 +1,21 @@
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#efc6c6ff';
+
+export const Colors = {
+  light: {
+    text: '#2d5166ff',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#687076',
+    tabIconDefault: '#4c5052ff',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#a6a9acff',
+    background: '#508ca9ff',
+    tint: tintColorDark,
+    icon: '#9BA1A6',
+    tabIconDefault: '#bdcddaff',
+    tabIconSelected: tintColorDark,
+  },
+};
